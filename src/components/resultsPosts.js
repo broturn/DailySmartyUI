19843aabdbb7c1df42671dbbc375728b7bf3d4f4
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-24 16:20:17 -0500

    put together resultsPosts Component

---
+++ src/components/resultsPosts.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+
+class ResultsPosts extends Component {
+    render() {
+        return(
+            <ul className="results-posts">
+                <div className="results-post__wrapper">
+                    <div className="results-posts__posts">
+                        Results go here
+                    </div>
+                </div>
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        state
+    }
+}
+
+
+export default connect(mapStateToProps)(ResultsPosts);
